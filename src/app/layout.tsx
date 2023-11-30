db08107d9eb4627c895211106a35915aec23c3ae
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-11-30 21:01:28 +0700

    chore: advance setup changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,34 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import Navbar from '@/components/navbar'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Mangabox",
-  description: "Free reading manga app",
-};
+  title: 'Mangabox',
+  description: 'Free reading manga app',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import { ModeToggle } from "@/components/dark-mode";
-
 export default function Home() {
   return (
-    <>
-      <main className="flex flex-col justify-center items-center min-h-screen">
-        <div className="absolute top-10">
-          <ModeToggle />
-        </div>
-        Welcome to mangadex
-      </main>
-    </>
-  );
+    <div className="flex min-h-screen items-center justify-center">Home</div>
+  )
 }

4bb02e030aca5a7d1e353866d7de1dfae59b0a87
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-11-30 21:05:28 +0700

    feat(ui): implements dark mode shadcn utilities

---
+++ src/lib/constant/navbar.ts
@@ -0,0 +1,19 @@
+export type NavigationItem = {
+  path: string
+  name: string
+}
+
+export const menuList: NavigationItem[] = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/trending',
+    name: 'Trending',
+  },
+  {
+    path: '/new',
+    name: 'New',
+  },
+]

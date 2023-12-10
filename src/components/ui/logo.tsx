281537a6b53c9ac453342ccb0a2b10664854fe9a
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-12-10 20:13:22 +0700

    refa: minor changes

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,9 +1,5 @@
 import Navbar from '../navbar'
 
 export default function Logo() {
-  return (
-    <div>
-      <Navbar showMenu={false} />
-    </div>
-  )
+  return <Navbar showMenu={false} />
 }

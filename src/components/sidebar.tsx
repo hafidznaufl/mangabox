ef137f6196794232371cfe61c9e160e2f71bdf60
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-12-10 19:22:38 +0700

    feat(component): add prep sidebar component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return (
+    <div className="absolute inset-y-0 left-0 h-screen py-16">
+      <div className="flex h-full w-20 flex-col items-center gap-4 rounded-r-2xl border-2 border-l-0 bg-foreground/5 backdrop-blur-lg transition-all hover:w-72"></div>
+    </div>
+  )
+}
